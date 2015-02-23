806fcf920c247eff303d7e241dbfb601c76044ad
Mandarava <569045298@qq.com>
2015-02-23 22:16:51 +0800

    动态绑定与多态

---
+++ myJavaWork/TestPolymorph.java
@@ -0,0 +1,83 @@
+package ¶¯Ì¬°ó¶¨Óë¼Ì³Ð;
+
+class Animal {
+	private String name;
+	Animal(String name) 
+	{
+		this.name=name;
+	}
+	
+	public void enjoy() 
+	{
+		System.out.println("½ÐÉù........");
+	}
+	
+}
+
+class Dog extends Animal {
+	private String furColor;
+	Dog(String n,String c)
+	{
+		super(n);
+		furColor = c;
+	}
+	
+	public void enjoy()						//Èç¹û²»ÖØÐ´Ôò²»»áÊä³ö¹·½ÐÉù £¬Êä³ö½ÐÉù
+	{
+		System.out.println("¹·½ÐÉù.......");
+	}
+}
+
+class Cat extends Animal {
+	private String eyesColor;
+	Cat(String n,String c) 
+	{
+		super(n);
+		eyesColor = c;
+	}
+	
+	public void enjoy() 
+	{
+		System.out.println("Ã¨½ÐÉù.......");
+	}
+}
+
+class Bird extends Animal {
+	Bird() 
+	{
+		super("bird");
+	}
+	
+	public void enjoy() 
+	{
+		System.out.println("Äñ½ÐÉù.......");
+	}
+}
+
+class Lady {
+	private String name;
+	private Animal pet;
+	Lady(String name,Animal pet) 
+	{
+		this.name=name;
+		this.pet=pet;
+	}
+	public void myPetEnjoy()
+	{
+		pet.enjoy();
+	}
+}
+
+public class TestPolymorph {
+	public static void main(String args[]) {
+		Cat c = new Cat("catname","blue");
+		Dog d = new Dog("dogname","black");
+		//Bird b = new Bird();
+		Lady L1 = new Lady("L1",c);
+		Lady L2 = new Lady("L2",d);
+		//Lady L3 = new Lady("L3",b);
+		L1.myPetEnjoy();
+		L2.myPetEnjoy();
+		//L3.myPetEnjoy();
+	}
+}
