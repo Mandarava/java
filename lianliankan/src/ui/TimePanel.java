7f2e6f14c94f0eaf8e93cfee6416ab6861934849
Mandarava <569045298@qq.com>
2015-06-27 22:35:30 +0800

    添加定时效果

--- lianliankan/src/ui/TimePanel.java
+++ lianliankan/src/ui/TimePanel.java
@@ -1,46 +1,28 @@
 package ui;
 
-import java.awt.BorderLayout;
-import java.awt.Checkbox;
-import java.awt.CheckboxGroup;
 import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.RandomAccessFile;
-import javax.swing.Box;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.Timer;
 
+import javax.swing.JPanel;
 
 class TimePanel extends JPanel {
 	int cnt = 0;
+	private static final int W_WIDTH = 1000;
 
+	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.setColor(Color.red);
-		g.fillRect(this.getWidth() - cnt, 0, cnt, this.getHeight());
+		g.fillRect(0, 0, cnt, 200);
+
 	}
 
 	public boolean isEnd() {
-		if (cnt++ > this.getWidth())
+		System.out.println(cnt);
+		if (cnt++ > W_WIDTH)
 			return true;
 		repaint();
 		return false;
 	}
+
 }
