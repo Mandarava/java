accbfc48463fb985ff1ec3c3cb9835449c8d4876
Mandarava <569045298@qq.com>
2016-06-29 22:54:30 +0800

    lazy-load

--- sh/test/junit/OneToOne.java
+++ sh/test/junit/OneToOne.java
@@ -55,12 +55,14 @@ public class OneToOne {
 			int id = 1;
 			session = HibernateUtil2.INSTANCE.getSessionFactory().getCurrentSession();
 			tx = session.beginTransaction();
+			// 一对一主对象不会懒加载
 			Person p = (Person) session.get(Person.class, id);
 			// System.out.print(p.getIdCard());
+			// 从对象会懒加载
+			// 主表不能有constrained=true,所以主表没有懒加载。  lazy!=false,constrained=true,fetch=select
 			// IdCard idCard = (IdCard) s.get(IdCard.class, id);
 			// System.out.println(idCard.getPerson());
 			tx.commit();
-
 		} catch (HibernateException e) {
 			if (tx != null) {
 				tx.rollback();
